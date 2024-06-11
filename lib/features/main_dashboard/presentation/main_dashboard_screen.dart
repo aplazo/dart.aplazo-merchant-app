9f5db0735f9e22a2cc8d1356df00ca61eb4815c0
Uziel Castillo <daviduziel@gmail.com>
2024-06-11 13:10:51 -0600

    Update main_dashboard_screen.dart

--- lib/features/main_dashboard/presentation/main_dashboard_screen.dart
+++ lib/features/main_dashboard/presentation/main_dashboard_screen.dart
@@ -25,39 +25,25 @@ class MainDashboardScreen extends StatefulWidget {
 class _MainDashboardScreenState extends State<MainDashboardScreen> {
   final shortcuts = [
     const Shortcut(
-        name: 'Panel', asset: 'assets/images/ic_panel.svg', moveTo: 'panel'),
+        name: 'Historicos',
+        asset: 'assets/images/ic_ventas.svg',
+        moveTo: 'ventas'),
     const Shortcut(
-        name: 'Ventas', asset: 'assets/images/ic_ventas.svg', moveTo: 'ventas'),
+        name: 'Pedidos',
+        asset: 'assets/images/ic_orders.svg',
+        moveTo: 'ordenes'),
     const Shortcut(
-        name: 'Registrar\nUsuario',
-        asset: 'assets/images/ic_registro.svg',
-        moveTo: 'registro'),
+        name: 'AplazoVersity',
+        asset: 'assets/images/ic_aplazoversity.svg',
+        moveTo: 'aplazoversity'),
     const Shortcut(
-        name: 'Escanear\nQR', asset: 'assets/images/ic_qr.svg', moveTo: 'qr'),
-    const Shortcut(
-        name: 'Estados de\ncuenta',
-        asset: 'assets/images/ic_edo_cuenta.svg',
-        moveTo: 'estados_cuenta'),
-    const Shortcut(
-        name: 'Info. del\ncomercio',
+        name: 'Configuración',
         asset: 'assets/images/ic_info_comercio.svg',
         moveTo: 'edo'),
     const Shortcut(
         name: 'Soporte',
         asset: 'assets/images/ic_soporte.svg',
         moveTo: 'soporte'),
-    const Shortcut(
-        name: 'AplazoVersity',
-        asset: 'assets/images/ic_aplazoversity.svg',
-        moveTo: 'aplazoversity'),
-    const Shortcut(
-        name: 'Simulador de pagos',
-        asset: 'assets/images/ic_simulador_pagos.svg',
-        moveTo: 'simulador'),
-    const Shortcut(
-        name: 'Ordenes',
-        asset: 'assets/images/ic_orders.svg',
-        moveTo: 'ordenes')
   ];
 
   @override
@@ -67,79 +53,79 @@ class _MainDashboardScreenState extends State<MainDashboardScreen> {
         children: [
           Expanded(
               child: SingleChildScrollView(
-                child: Column(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(
                   children: [
-                    const SizedBox(
-                      height: 80,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: AplazoText(
-                              textProps: TextProps(
-                                  text: 'Hola, Adidas',
-                                  type: TextType.headlineSize24Weight700,
-                                  multiAligment: TextAlign.start,
-                                  aligment: Alignment.centerLeft,
-                                  paddingHorizontal: 16)),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: SvgPicture.asset(
-                              MainDashboardScreen.merchantImage),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                            child: Column(
-                              children: [
-                                AplazoText(
-                                    textProps: TextProps(
-                                        text: 'Tus ventas de hoy',
-                                        type: TextType.headlineSize14Weight400,
-                                        multiAligment: TextAlign.start,
-                                        aligment: Alignment.centerLeft,
-                                        paddingHorizontal: 16)),
-                                AplazoText(
-                                    textProps: TextProps(
-                                        text: '\$32,123,200.00',
-                                        type: TextType.headlineSize24Weight700,
-                                        multiAligment: TextAlign.start,
-                                        aligment: Alignment.centerLeft,
-                                        paddingHorizontal: 16))
-                              ],
-                            )),
-                        const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            child: Icon(
-                              Icons.chevron_right,
-                            ))
-                      ],
+                    Expanded(
+                      child: AplazoText(
+                          textProps: TextProps(
+                              text: 'Hola, Adidas',
+                              type: TextType.headlineSize24Weight700,
+                              multiAligment: TextAlign.start,
+                              aligment: Alignment.centerLeft,
+                              paddingHorizontal: 16)),
                     ),
                     Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: GridView.builder(
-                          itemCount: shortcuts.length,
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 4,
-                          ),
-                          itemBuilder: (context, index) =>
-                              rowShortcut(shortcuts[index], (shortcut) {
-                                handleShortcut(shortcut, context);
-                              })),
-                    ),
-                    const AplazoAgentProgressCard(),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child:
+                          SvgPicture.asset(MainDashboardScreen.merchantImage),
+                    )
                   ],
                 ),
-              )),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Column(
+                      children: [
+                        AplazoText(
+                            textProps: TextProps(
+                                text: 'Tus ventas de hoy',
+                                type: TextType.headlineSize14Weight400,
+                                multiAligment: TextAlign.start,
+                                aligment: Alignment.centerLeft,
+                                paddingHorizontal: 16)),
+                        AplazoText(
+                            textProps: TextProps(
+                                text: '\$32,123,200.00',
+                                type: TextType.headlineSize24Weight700,
+                                multiAligment: TextAlign.start,
+                                aligment: Alignment.centerLeft,
+                                paddingHorizontal: 16))
+                      ],
+                    )),
+                    const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Icon(
+                          Icons.chevron_right,
+                        ))
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: GridView.builder(
+                      itemCount: shortcuts.length,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                      ),
+                      itemBuilder: (context, index) =>
+                          rowShortcut(shortcuts[index], (shortcut) {
+                            handleShortcut(shortcut, context);
+                          })),
+                ),
+                const AplazoAgentProgressCard(),
+              ],
+            ),
+          )),
           AplazoButton(
               buttonProps: ButtonProps(
                   text: 'Nuevo pedido', buttonType: ButtonType.primary),
@@ -174,16 +160,20 @@ class _MainDashboardScreenState extends State<MainDashboardScreen> {
         height: 100,
         child: Column(
           children: [
-          SvgPicture.asset(shortcut.asset, height: 50, width: 50,),
+            SvgPicture.asset(
+              shortcut.asset,
+              height: 50,
+              width: 50,
+            ),
             const SizedBox(
               height: 8,
             ),
-          AplazoText(
-              textProps: TextProps(
-                  text: shortcut.name,
-                  type: TextType.headlineSize12Weight500))
-        ],
-      ),
+            AplazoText(
+                textProps: TextProps(
+                    text: shortcut.name,
+                    type: TextType.headlineSize12Weight500))
+          ],
+        ),
       ),
     );
   }

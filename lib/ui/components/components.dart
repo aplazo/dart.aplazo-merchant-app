99a8d86c5624cb444d5dc5d1a96a3473680b2ddd
Uziel Castillo <daviduziel@gmail.com>
2024-06-11 17:59:08 -0600

    corrected loader

--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -8,6 +8,6 @@ export 'package:flutter_posui_pocket/ui/components/alerts/alert.dart';
 // export 'package:flutter_posui_pocket/ui/components/webviews/webviews.dart';
 // export 'package:flutter_posui_pocket/ui/components/navigations/navigations.dart';
 // export 'package:flutter_posui_pocket/ui/components/inputs/inputs.dart';
-// export 'package:flutter_posui_pocket/ui/components/loaders/loaders.dart';
+export 'package:flutter_posui_pocket/ui/components/loaders/loaders.dart';
 export 'package:flutter_posui_pocket/ui/components/texts/texts.dart';
 export 'package:flutter_posui_pocket/ui/components/buttons/buttons.dart';

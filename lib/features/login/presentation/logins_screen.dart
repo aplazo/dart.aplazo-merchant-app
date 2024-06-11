32659c4a9a7bced1a2dab09e42a2f055b37e87f7
Uziel Castillo <daviduziel@gmail.com>
2024-06-11 13:17:42 -0600

    Update logins_screen.dart

--- lib/features/login/presentation/logins_screen.dart
+++ lib/features/login/presentation/logins_screen.dart
@@ -31,107 +31,109 @@ class _LoginScreenState extends State<LoginScreen> {
         child: BlocConsumer<LoginBloc, LoginState>(
           builder: (context, state) {
             if (state is! LoginLoading) {
-            return Scaffold(
-              appBar: AplazoNavbar(
-                  navbarProps:
-                  NavbarProps(title: 'Inicia sesión', isBackEnable: false)),
-              body: SafeArea(
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    AplazoText(
-                        textProps: TextProps(
-                            text: 'Bienvenido',
-                            type: TextType.headlineSize32Weight700,
-                            multiAligment: TextAlign.start,
-                            aligment: Alignment.centerLeft,
-                            paddingHorizontal: 16)),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    AplazoText(
-                        textProps: TextProps(
-                            text: 'Inicia sesión como comerciante',
-                            type: TextType.headlineSize16Weight400,
-                            multiAligment: TextAlign.start,
-                            aligment: Alignment.centerLeft,
-                            paddingHorizontal: 16)),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    AplazoTextField(
-                        textFieldProps: TextFieldProps(
+              return Scaffold(
+                appBar: AplazoNavbar(
+                    navbarProps: NavbarProps(
+                        title: 'Inicia sesión', isBackEnable: false)),
+                body: SafeArea(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      AplazoText(
+                          textProps: TextProps(
+                              text: 'Bienvenido',
+                              type: TextType.headlineSize32Weight700,
+                              multiAligment: TextAlign.start,
+                              aligment: Alignment.centerLeft,
+                              paddingHorizontal: 16)),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      AplazoText(
+                          textProps: TextProps(
+                              text: 'Inicia sesión como comerciante',
+                              type: TextType.headlineSize16Weight400,
+                              multiAligment: TextAlign.start,
+                              aligment: Alignment.centerLeft,
+                              paddingHorizontal: 16)),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      AplazoTextField(
+                          textFieldProps: TextFieldProps(
                               hintText: 'Usuario',
                               label: 'Ingresa tu usuario',
                               textFieldType: TextFieldType.email),
-                        onChanged: (text) {},
-                        controller: emailController),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    StatefulBuilder(builder: (context, state) {
-                      return AplazoTextField(
-                          textFieldProps: TextFieldProps(
-                              hintText: 'Password',
-                              showPassword: showPassword,
-                              suffixCustomWidget: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    showPassword = !showPassword;
-                                  });
-                                },
-                                icon: Icon(showPassword
-                                    ? Icons.remove_red_eye_rounded
-                                    : Icons.add_circle_outline),
-                              ),
-                              isPassword: true,
-                              label: 'Ingresa tu contraseña',
-                              textFieldType: TextFieldType.normal),
                           onChanged: (text) {},
-                          controller: passwordController);
-                    }),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    AplazoButton(
-                        buttonProps: ButtonProps(
-                            text: 'Continuar', buttonType: ButtonType.primary),
-                        onPressed: () {
-                          BlocProvider.of<LoginBloc>(context).add(
-                              UserLoginEvent(user: emailController.text,
-                                  password: passwordController.text));
-                        }),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    AplazoText(
-                        textProps: TextProps(
-                            text: 'OLVIDE MI CONTRASEÑA',
-                            type: TextType.headlineSize16Weight700)),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    AplazoText(
-                        textProps: TextProps(
-                            text: '¿No tienes cuenta de comercio?',
-                            type: TextType.headlineSize16Weight700)),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    AplazoButton(buttonProps: ButtonProps(
-                        text: 'Crea tu cuenta comercial',
-                        buttonType: ButtonType.secondary), onPressed: () {
-
-                    })
-                  ],
+                          controller: emailController),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      StatefulBuilder(builder: (context, state) {
+                        return AplazoTextField(
+                            textFieldProps: TextFieldProps(
+                                hintText: 'Password',
+                                showPassword: showPassword,
+                                suffixCustomWidget: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      showPassword = !showPassword;
+                                    });
+                                  },
+                                  icon: Icon(showPassword
+                                      ? Icons.remove_red_eye_rounded
+                                      : Icons.add_circle_outline),
+                                ),
+                                isPassword: true,
+                                label: 'Ingresa tu contraseña',
+                                textFieldType: TextFieldType.normal),
+                            onChanged: (text) {},
+                            controller: passwordController);
+                      }),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      AplazoButton(
+                          buttonProps: ButtonProps(
+                              text: 'Continuar',
+                              buttonType: ButtonType.primary),
+                          onPressed: () {
+                            BlocProvider.of<LoginBloc>(context).add(
+                                UserLoginEvent(
+                                    user: emailController.text,
+                                    password: passwordController.text));
+                          }),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      const Divider(
+                        height: 60,
+                        thickness: 5,
+                        indent: 20,
+                        endIndent: 0,
+                        color: Colors.white,
+                      ),
+                      AplazoText(
+                          textProps: TextProps(
+                              text: '¿No tienes cuenta de comercio?',
+                              type: TextType.headlineSize16Weight700)),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      AplazoButton(
+                          buttonProps: ButtonProps(
+                              text: 'Crea tu cuenta comercial',
+                              buttonType: ButtonType.secondary),
+                          onPressed: () {})
+                    ],
+                  ),
                 ),
-              ),
-            );
-          } else {
-            return const AplazoLoader();
-          }
+              );
+            } else {
+              return const AplazoLoader();
+            }
           },
           listener: (context, state) {
             if (state is UserLogged) {

2cf412970c2adbc5b2d46bad29aac958245a2c46
Manuel Lopez <manuel.lopez@aplazo.mx>
2024-06-12 15:46:02 -0600

    validation botton

--- lib/features/simple_register/presentation/simple_register_screen.dart
+++ lib/features/simple_register/presentation/simple_register_screen.dart
@@ -206,14 +206,22 @@ class SimpleRegisterView extends StatelessWidget {
                     buttonProps: ButtonProps(
                         text: 'Continuar', buttonType: ButtonType.primary),
                     onPressed: () {
-                      BlocProvider.of<RegisterBloc>(context).add(
-                          MerchantRegisterEvent(
-                              bussinessNameController.text,
-                              industryController.text,
-                              rfcController.text,
-                              phoneController.text,
-                              emailController.text,
-                              clabeController.text));
+                      var allFieldsFilled = emailController.text.isNotEmpty &&
+                          bussinessNameController.text.isNotEmpty &&
+                          rfcController.text.isNotEmpty &&
+                          industryController.text.isNotEmpty &&
+                          phoneController.text.isNotEmpty &&
+                          clabeController.text.isNotEmpty;
+                      if (allFieldsFilled) {
+                        BlocProvider.of<RegisterBloc>(context).add(
+                            MerchantRegisterEvent(
+                                bussinessNameController.text,
+                                industryController.text,
+                                rfcController.text,
+                                phoneController.text,
+                                emailController.text,
+                                clabeController.text));
+                      }
                     }),
               ],
             ),
